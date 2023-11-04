ca7fa5646c3f005259d5eab68a03cee45228899c
Bakhyt Sembayev <bahytsembaev@gmail.com>
2023-11-05 01:48:58 +0600

    мой background и  меню из  feature/new message

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import { ShapeModule } from './modules/shape.module';
+import { BackgroundModule } from './modules/background.module';
 
 const shapeModule = new ShapeModule();
 
@@ -16,3 +17,85 @@ document.addEventListener('DOMContentLoaded', () => {
     console.error('Кнопка для генерации фигур не найдена!');
   }
 });
+
+const backgroundModule = new BackgroundModule('background', 'Change background color');
+
+// код из ветки feature/new message
+console.log('hallo dude');
+
+let menuContainer = null; // Переменная, хранящая контейнер меню
+
+// Обработчик события при открытии контекстного меню
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault(); // Отменяем стандартное контекстное меню
+
+  // Проверяем, открыто ли уже меню
+  if (menuContainer) {
+    document.body.removeChild(menuContainer); // Удаляем предыдущее меню
+  }
+
+  // Создаем контейнер для меню
+  menuContainer = document.createElement('ul');
+  menuContainer.className = 'menu';
+
+  // Создаем пункты меню
+  const items = [
+    { label: 'Считать клики', action: countClicks },
+    { label: 'Случайная фигура', action: randomShape },
+    { label: 'Случайный звук', action: randomSound },
+    { label: 'Случайный цвет', action: randomColor },
+    { label: 'Вызвать сообщение', action: showMessage }
+  ];
+
+  // Добавляем пункты меню в контейнер
+  items.forEach(function(item) {
+    const menuItem = document.createElement('li');
+    menuItem.className = 'menu-item';
+    menuItem.textContent = item.label;
+    menuItem.addEventListener('click', item.action);
+    menuContainer.appendChild(menuItem);
+  });
+
+  // Позиционируем контекстное меню на экране
+  menuContainer.style.left = event.clientX + 'px';
+  menuContainer.style.top = event.clientY + 'px';
+
+  // Добавляем класс для отображения меню
+  menuContainer.classList.add('open');
+
+  // Добавляем контекстное меню на экран
+  document.body.appendChild(menuContainer);
+});
+
+// Обработчик события при клике на другую область экрана или повторном нажатии правой кнопкой мыши
+document.addEventListener('click', function(event) {
+  if (event.button !== 2 || !menuContainer) return; // Проверяем, не является ли нажатие другой кнопкой или меню не открыто
+
+  document.body.removeChild(menuContainer); // Удаляем меню
+  menuContainer = null; // Сбрасываем переменную контейнера меню
+});
+  
+  // Функции для выполнения действий при выборе пунктов меню
+  function countClicks() {
+    // Действие при выборе пункта "Считать клики"
+    console.log('Считать клики');
+  }
+  
+  function randomShape() {
+    // Действие при выборе пункта "Случайная фигура"
+    console.log('Случайная фигура');
+  }
+  
+  function randomSound() {
+    // Действие при выборе пункта "Случайный звук"
+    console.log('Случайный звук');
+  }
+  
+  function randomColor() {    
+    backgroundModule.trigger(document.body);
+  }
+  
+  function showMessage() {
+    // Действие при выборе пункта "Вызвать сообщение"
+    console.log('Вызвать сообщение');
+  }
