61b3bc3a5d7d6df748160935735d26168e775b78
Bakhyt Sembayev <bahytsembaev@gmail.com>
2023-11-04 13:55:17 +0600

    generateShape-1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-import './styles.css'
+import './styles.css';
+import { ShapeModule } from './modules/shape.module';
+
+const shapeModule = new ShapeModule();
+
+function triggerShapeModule() {
+  shapeModule.trigger();
+}
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const generateShapeButton = document.getElementById('generateShapeButton');
+  
+  if (generateShapeButton) {
+    generateShapeButton.addEventListener('click', triggerShapeModule);
+  } else {
+    console.error('Кнопка для генерации фигур не найдена!');
+  }
+});
--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,39 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module';
+import { randomNumber } from '../utils';
 
 export class ShapeModule extends Module {
+  constructor() {    
+    super('shapeModule', 'Shape');
+  }
 
+  trigger() {
+    
+    const shape = document.createElement('div');
+        
+    const size = randomNumber(50, 200); 
+    const color = `#${Math.floor(Math.random()*16777215).toString(16)}`; 
+    const positionX = randomNumber(0, window.innerWidth - size);
+    const positionY = randomNumber(0, window.innerHeight - size);
+
+    shape.style.width = `${size}px`;
+    shape.style.height = `${size}px`;
+    shape.style.backgroundColor = color;
+    shape.style.position = 'absolute';
+    shape.style.left = `${positionX}px`;
+    shape.style.top = `${positionY}px`;
+    shape.style.borderRadius = size > 100 ? '50%' : '0'; 
+
+    document.body.appendChild(shape);
+
+    shape.addEventListener('click', () => {
+      shape.remove();
+    });
+    
+    setTimeout(() => {
+      shape.remove();
+    }, 5000); 
+  }
 }
+
+const shapeModule = new ShapeModule();
+shapeModule.trigger(); 
